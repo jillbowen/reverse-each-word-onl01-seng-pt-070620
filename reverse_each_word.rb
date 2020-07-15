2a2624d00154a80ea79017e3353c99bbd085850f
Jill Short <jillian.b.short@gmail.com>
2020-07-15 20:44:09 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 def reverse_each_word(sentence)
   array = []
-  reverse_each_word(sentence).each do |reverse|
+  sentence.split
+  sentence.each do |reverse|
     array << "#{sentence.reverse}"
   end
   array.join( )
