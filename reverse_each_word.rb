0056bca0103b6adf123ae8a8af7b6e5bbdefb0c1
Jill Short <jillian.b.short@gmail.com>
2020-07-15 20:43:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence)
   array = []
-  sentence.each do |reverse|
+  reverse_each_word(sentence).each do |reverse|
     array << "#{sentence.reverse}"
   end
   array.join( )
