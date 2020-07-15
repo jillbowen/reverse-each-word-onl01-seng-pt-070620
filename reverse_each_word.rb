f3a0ef93069e8342f8fa0af7345b43a07757b566
Jill Short <jillian.b.short@gmail.com>
2020-07-15 21:10:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   array = []
   #sentence_split = sentence.split
   sentence.split.each do |reverse|
-    array << "#{sentence.reverse!}"
+    array << "#{sentence.split.reverse!}"
   end
   array.join( )
 end
